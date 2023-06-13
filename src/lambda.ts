33adb709e9d73c58f27a5e0ad1a4d94b3eb56f91
Andrey Chausenko <andrey@memz.co>
2023-06-13 16:59:02 +1000

    Fix wrapped handler return type - should be Promise

--- src/lambda.ts
+++ src/lambda.ts
@@ -3,24 +3,25 @@ import { readJsonFile } from "./misc";
 
 const { name, version } = readJsonFile("package.json") || {};
 
-export const createHandler = (handler: (event?: any, context?: any) => any) => async (event?: any, context?: any) =>
-  withContext(async () => {
-    const { logger } = getContext();
+export const createHandler =
+  (handler: (event?: any, context?: any) => Promise<any>) => async (event?: any, context?: any) =>
+    withContext(async () => {
+      const { logger } = getContext();
 
-    logger.info(`Running ${name || "unknown"} (${version || "unknown"}) for`, { event, context });
+      logger.info(`Running ${name || "unknown"} (${version || "unknown"}) for`, { event, context });
 
-    logger.setContext({
-      service: name,
-      version,
+      logger.setContext({
+        service: name,
+        version,
+      });
+
+      try {
+        const result = await handler(event, context);
+
+        logger.info("Successfully processed event", { result });
+        return result;
+      } catch (error) {
+        logger.error("Failed to process event", { error });
+        throw error;
+      }
     });
-
-    try {
-      const result = handler(event, context);
-
-      logger.info("Successfully processed event", { result });
-      return result;
-    } catch (error) {
-      logger.error("Failed to process event", { error });
-      throw error;
-    }
-  });
