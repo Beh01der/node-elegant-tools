f38633d3f12a077261fe3df8acc9ea18ced416bf
Andrey Chausenko <andrey@memz.co>
2023-06-13 17:47:07 +1000

    Fix tests

--- src/lambda.test.ts
+++ src/lambda.test.ts
@@ -2,7 +2,7 @@ import { createHandler } from "./lambda";
 
 describe("lambda", () => {
   it("should work", async () => {
-    const handler = createHandler((event, context) => (`${event}+${context}`));
+    const handler = createHandler(async (event, context) => (`${event}+${context}`));
     const result = await handler("event", "context");
 
     expect(result).toEqual("event+context");
