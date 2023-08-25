8a80306ba21e9f00188e6b2ac6a009da3cfe1657
Andrey Chausenko <andrey@memz.co>
2023-08-25 11:56:19 +1000

    Use context.getRemainingTimeInMillis to update flush timeout in AWS lambda

--- src/context.ts
+++ src/context.ts
@@ -1,14 +1,14 @@
 import { AsyncLocalStorage } from "node:async_hooks";
 import { ContextAwareLogger } from "./logger";
 
-type CallContext = {
+export type CallContext = {
   logger: ContextAwareLogger;
 };
 
 const asyncLocalStorage = new AsyncLocalStorage<CallContext>();
 
-export const withContext = async <T>(wrapped: () => T) => {
-  const context = { logger: new ContextAwareLogger() };
+export const withContext = async <T>(wrapped: () => T, newContext?: CallContext) => {
+  const context = newContext || { logger: new ContextAwareLogger() };
 
   try {
     return await asyncLocalStorage.run(context, wrapped);
--- src/lambda.ts
+++ src/lambda.ts
@@ -1,12 +1,20 @@
 import { v4 as uuidv4 } from "uuid";
+import { config } from "./config";
 import { getContext, withContext } from "./context";
+import { ContextAwareLogger } from "./logger";
 import { readJsonFile } from "./misc";
 
 const { name, version } = readJsonFile("package.json") || {};
 
 export const createHandler =
-  (handler: (event?: any, context?: any) => Promise<any>) => async (event?: any, context?: any) =>
-    withContext(async () => {
+  (handler: (event?: any, context?: any) => Promise<any>) => async (event?: any, context?: any) => {
+    let flushTimeout = config.flushTimeout;
+    if (context.getRemainingTimeInMillis) {
+      // for AWS Lambda we use min of configured timeout and remaining time but not less than 1 second
+      flushTimeout = Math.max(1, Math.min(flushTimeout, Math.floor(context.getRemainingTimeInMillis() / 1000) - 1));
+    }
+
+    return withContext(async () => {
       const { logger } = getContext();
 
       logger.info(`Running ${name || "unknown"} (${version || "unknown"})`, { event, context });
@@ -28,4 +36,5 @@ export const createHandler =
         logger.error("Failed to process event", error);
         throw error;
       }
-    });
+    }, { logger: new ContextAwareLogger({ flushTimeout }) });
+  };
